3114d6b34c3c6b3078fd67cddbe168ea11953eaa
sital-tharu <sitatharu769@gmail.com>
2025-03-02 17:58:19 +0530

    hide passwords

--- Enter.py
+++ Enter.py
@@ -1,29 +1,45 @@
-master_pwd = input("What is the master password?")
+from cryptography.fernet import Fernet
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+# Uncomment the line below to generate the key file, then comment it out again after the key is created
+write_key()
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
 
 def view():
-    with open('password.txt', 'r') as f:
+    with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
-            if '|' in data:
-                user, passw = data.split("|")
-                print("User:", user, "| Password:", passw)
-            else:
-                print(f"Skipping invalid line: {line}")
+            user, passw = data.split("|")
+            print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input('Account Name: ')
     pwd = input("Password: ")
 
-    with open('password.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit ").lower()
+    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
     if mode == "q":
         break
+
     if mode == "view":
         view()
     elif mode == "add":
         add()
     else:
         print("Invalid mode.")
+        continue
